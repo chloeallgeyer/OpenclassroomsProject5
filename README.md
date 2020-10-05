14d975a9eedc623ee84182f8be367dcaffea196e
Chloe Allgeyer <chloe.allgeyer@gmail.com>
2020-10-05 19:47:48 +0200

    dynamic cards with JS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OpenclassroomsProject5
+
+```
+cd C:\Users\clo\Documents\formationoc\P5\OpenclassroomsProject5-Back
+```
--- script.js
+++ script.js
@@ -1,19 +1,61 @@
-let norbertPrice = document.getElementById('price');
-let norbertName = document.getElementById('name');
-let norbertDescription = document.getElementById('description');
+const host = 'http://localhost:3000/';
 
-
-const makeAjaxCall = () => {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {   
-        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
-            var response = JSON.parse(request.responseText);
-            console.log(response.price);
-            norbertPrice.innerHTML = response.price;
+const ajaxRequest = (method, url) => {
+    return new Promise( (resolve, reject) => {
+        // instanciation de la classe XMLHttpRequest 
+        const request = new XMLHttpRequest();
+        // appel de la méthode onreadystatechange
+        request.onreadystatechange = function() {   
+            if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+                const response = JSON.parse(request.responseText);
+                resolve(response);
+            } else if (request.status > 400) {
+                // Gestion des erreurs server > 400
+                reject(request.status)
+            }
         }
-    }
-    request.open("GET", "http://localhost:3000/api/teddies/5be9c8541c9d440000665243");
-    request.send();
-};
+        // appel de la méthode onerror (autres types d'erreur)
+        request.onerror = (error) => reject(error);
+        //  appel des méthodes open et send
+        request.open(method, url);
+        request.send();
+    })
+}
 
-makeAjaxCall();
+const renderList = (data) => {
+    // récupération de l'élément cible (conteneur des cards à remplir avec les infos de chaque ourson)
+    const target = document.getElementById('target');
+    // On boucle sur chaque élement de la réponse de l'API (tableau des oursons)
+    for (let element of data) {
+        // déclaration et initialisation de la card
+        const card = `
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="${element.imageUrl}" class="card-img-top" alt="Ours en peluche">
+                    <div class="card-body">
+                        <h5 class="card-title">Ourson ${element.name}</h5>
+                        <p class="card-text">${element.description}</p>
+                        <p>Prix : ${element.price}€</p>
+                        <a href="produit.html?id=${element._id}" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        `;
+        console.log("element", element);
+        // ajoute à l'élément cible (target) la card déclarée ci dessus
+        target.insertAdjacentHTML('beforeend', card);
+    }
+}
+
+const renderError = (error) => {
+    const target = document.getElementById('target');
+    // TODO: créer variable contenant structure message d'erreur + bouton refresh
+    target.insertAdjacentHTML('beforeend', `<h2>Une erreur est survenue: impossible de charger la page</h2>`);
+    console.log(`erreur survenue : ${error}`);
+}
+// appel de la base de données et display de la liste de produits (.then) et de la page d'erreur (.catch)
+ajaxRequest("GET", `${host}api/teddies/`)
+    .then( (data) => renderList(data))
+    .catch( (error) => renderError(error));
+
+
